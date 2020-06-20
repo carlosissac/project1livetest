538900373d1a5ddc0ee52b4f974107fcd557d01e
Issac Angulo <issac.angulo@gmail.com>
2020-06-19 20:52:57 -0700

    livetest5

--- assets/apiHandler.js
+++ assets/apiHandler.js
@@ -31,7 +31,8 @@ let apiHandler = {
 
         //website URL 
         //https://us-zipcode.api.smartystreets.com/lookup?key=3550738597428721952&city=mountain+view&state=CA&zipcode=94035"
-        let queryURL = `https://us-zipcode.api.smartystreets.com/lookup?key=${keyS}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
+        //let queryURL = `https://us-zipcode.api.smartystreets.com/lookup?key=${keyS}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
+        let queryURL = `https://us-zipcode.api.smartystreets.com/street-address?key=${keyS}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
 
         console.log(keyS);
         console.log(queryURL);
