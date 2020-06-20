3a4b064a8ef8fadd6dfd7f14b7f7b101ffadfff9
Issac Angulo <issac.angulo@gmail.com>
2020-06-19 21:09:56 -0700

    livetest7

--- assets/apiHandler.js
+++ assets/apiHandler.js
@@ -12,7 +12,7 @@ let apiHandler = {
     du : dynamicUi, 
 
     loadApiKeys: function () {
-        this.keySmarty = `3738003473505566`;
+        //this.keySmarty = `3738003473505566`;
         this.appIDSmarty = JSON.parse(localStorage.getItem("appIDSmartyStreets"));
         this.authTokenSmarty = JSON.parse(localStorage.getItem("authTokenSmartyStreets"));
         this.appKeyGoogle = JSON.parse(localStorage.getItem("appKeyGoogle"));
@@ -27,14 +27,14 @@ let apiHandler = {
         let zip = JSON.parse(localStorage.getItem("zip"));
 
         //Host URL 
-        //let queryURL = `https://us-street.api.smartystreets.com/street-address?auth-id=${this.appIDSmarty}&auth-token=${this.authTokenSmarty}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
+        let queryURL = `https://us-street.api.smartystreets.com/street-address?auth-id=${this.appIDSmarty}&auth-token=${this.authTokenSmarty}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
 
         //website URL 
         //https://us-zipcode.api.smartystreets.com/lookup?key=3550738597428721952&city=mountain+view&state=CA&zipcode=94035"
         //let queryURL = `https://us-zipcode.api.smartystreets.com/lookup?key=${keyS}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
-        let queryURL = `https://us-zipcode.api.smartystreets.com/street-address?key=${keyS}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
+        //let queryURL = `https://us-zipcode.api.smartystreets.com/street-address?key=${keyS}&candidates=1&match=invalid&street=${add1}&street2=${add2}&city=${city}&state=${state}&zipcode=${zip}`;
 
-        console.log(keyS);
+        //console.log(keyS);
         console.log(queryURL);
 
         let promise = await Promise.all([
@@ -112,8 +112,8 @@ let apiHandler = {
 
 function apiKeySet() {
     /*TEMP*/
-    localStorage.setItem("appIDSmartyStreets", JSON.stringify("e9d70270-b768-6b33-d14c-11207bb6d4f3"));
-    localStorage.setItem("authTokenSmartyStreets", JSON.stringify("X5JfMEr6aPM3ud7WdpNv"));
+    localStorage.setItem("appIDSmartyStreets", JSON.stringify("64dbbfab-9906-6e7c-23ae-bf82bafd8720"));
+    localStorage.setItem("authTokenSmartyStreets", JSON.stringify("6BKo0kIp3IwZD3o3wt7B"));
     localStorage.setItem("appKeyGoogle", JSON.stringify("AIzaSyDYsucFLhfwF4iEpT9CrAD7rCFdUvrQ87E"));
     /*TEMP*/
 }
